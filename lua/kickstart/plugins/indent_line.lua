0ae6c423a68e42a8cff563243f3d0cfd7d4e4ff2
Facundo Tuñas <154563924+Uruwhy1@users.noreply.github.com>
2025-10-03 02:17:01 -0300

    set up nvim config

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -1,9 +1,26 @@
 return {
-  { -- Add indentation guides even on blank lines
+  {
     'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
     main = 'ibl',
-    opts = {},
+    config = function()
+      local hooks = require 'ibl.hooks'
+      local catppuccin = require('catppuccin.palettes').get_palette()
+
+      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+        -- Normal indent guides
+        vim.api.nvim_set_hl(0, 'IndentGuide', { fg = catppuccin.surface0 })
+
+        -- Focused scope without underline
+        vim.api.nvim_set_hl(0, 'IndentScope', {
+          fg = catppuccin.red,
+          underline = false,
+        })
+      end)
+
+      require('ibl').setup {
+        indent = { highlight = { 'IndentGuide' } },
+        scope = { highlight = { 'IndentScope' }, show_start = false, show_end = false },
+      }
+    end,
   },
 }
